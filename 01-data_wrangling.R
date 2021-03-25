fdd614ea9a04c4586dd1d8c4c1d8bb1c5c1366db
Fabbiologia <favoretto.fabio@gmail.com>
2021-03-25 14:53:51 -0700

    added complex graphs

--- 01-data_wrangling.R
+++ 01-data_wrangling.R
@@ -5,6 +5,8 @@ library(tidyverse)
 library(readxl)
 library(googlesheets4)
 library(stringr)
+library("gplots")
+
 
 # Loading data ------------------------------------------------------------
 
@@ -28,9 +30,23 @@ data_graph <- elemental %>%
         mutate(element = word(element,1,sep = "\\_"))
 
 
+# for the heatmap
 
+mtx <- elemental %>% 
+        filter(!is.na(Muestra)) %>% 
+        janitor::clean_names() %>% 
+        select(contains("perc")) %>% 
+        as.matrix() %>% 
+        scale()
+
+rownames(mtx) <- elemental$Muestra
+
+colnames(mtx) <- word(colnames(mtx),1,sep = "\\_")
 
 # Data plotting -----------------------------------------------------------
+
+
+# Exploratory graph
 
 data_graph %>% 
         ggplot(aes(x = reorder(element, percent), y = percent)) +
@@ -43,10 +59,41 @@ data_graph %>%
              y = "Element name (Abbr.)", 
              title = "Element concentration in biomineralization", 
              subtitle = "Coralline algae")
-        
+
+# Heatmap example
+
+heatmap.2(mtx, 
+          Rowv = T,
+          scale = "none", 
+          col = bluered(100), 
+          trace = "none", 
+          density.info = "none", 
+          )
 
 
+## More complex heatmap example
 
+
+# website reference https://jokergoo.github.io/ComplexHeatmap-reference/book/
+
+library(ComplexHeatmap)
+library(circlize)
+
+col_fun <- colorRamp2(c(-2.5, 0, 2.5), c("dodgerblue4", "white", "firebrick")) #select colour ramp
+
+Heatmap(mtx, 
+        name = "Scale", 
+        cluster_columns = FALSE,
+        col = col_fun,
+        clustering_distance_rows = "pearson",
+        row_split = factor(elemental$Depth, 
+                           levels = c("1", "2"), 
+                           labels = c("Winter", "Summer")),
+        row_title_gp = gpar(fill = c("#D0EAE1", "#EBB6AC")),
+        rect_gp = gpar(col = "white", lwd = 2),
+        row_dend_reorder = TRUE,
+        #row_km = 2,
+        row_names_gp = gpar(fontsize = 5)) 
 
 
 
